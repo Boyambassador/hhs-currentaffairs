379fa88658a351ee23fa98e005f5abfead111af6
root <root@intelligentafrica.co.zw>
2022-12-08 12:50:36 +0000

    revamp

--- myproject/settings.py
+++ myproject/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY =  config['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['127.0.0.1','localhost','192.168.0.1','.ngrok.io','*','intelligentafrica.co.zw','13.244.153.234'
 ]
 
@@ -65,6 +65,7 @@ INSTALLED_APPS = [
     'base',
     'rest_framework',
     "corsheaders",
+    'theme_soft_design',
 ]
 
 MIDDLEWARE = [
@@ -115,7 +116,6 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     } }
-}
 
 # DATABASES = {
 #     'default': {
